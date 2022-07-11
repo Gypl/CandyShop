5a8eb7a656cf731960e0768e74514e993c5545df
Gypl <lavlovandrejj@gmail.com>
2022-07-12 03:02:30 +0400

    Added localization files

--- src/main/java/com/company/candyshop/screen/confectionery/ConfectioneryEdit.java
+++ src/main/java/com/company/candyshop/screen/confectionery/ConfectioneryEdit.java
@@ -2,9 +2,11 @@ package com.company.candyshop.screen.confectionery;
 
 import com.company.candyshop.app.ConfectioneryService;
 import com.company.candyshop.app.OrderService;
+import com.company.candyshop.entity.FlowSheet;
 import com.company.candyshop.screen.order.OrderEdit;
 import io.jmix.core.Messages;
 import io.jmix.ui.Notifications;
+import io.jmix.ui.component.HasValue;
 import io.jmix.ui.screen.*;
 import com.company.candyshop.entity.Confectionery;
 import org.slf4j.Logger;
@@ -21,12 +23,18 @@ public class ConfectioneryEdit extends StandardEditor<Confectionery> {
     private ConfectioneryService confectioneryService;
     @Autowired
     private Notifications notifications;
+
+    @Subscribe("confecrioneryNameField")
+    public void onConfecrioneryNameFieldValueChange(HasValue.ValueChangeEvent<FlowSheet> event) {
+        if (getEditedEntity().getConfectioneryName().getCandyShop() != null)
+            getEditedEntity().setCandyShop(getEditedEntity().getConfectioneryName().getCandyShop());
+    }
     @Autowired
     private Messages messages;
     @Subscribe
     public void onBeforeCommitChanges(BeforeCommitChangesEvent event) {
         if (!confectioneryService.consumeResources(getEditedEntity())) {
-            notifications.create().withCaption(messages.getMessage(("msg://localization/denyConfectionery")))
+            notifications.create().withCaption(messages.getMessage(("localization/denyConfectionery")))
                     .withType(Notifications.NotificationType.WARNING)
                     .show();
             event.preventCommit();
--- src/main/java/com/company/candyshop/screen/order/OrderEdit.java
+++ src/main/java/com/company/candyshop/screen/order/OrderEdit.java
@@ -4,6 +4,7 @@ import com.company.candyshop.app.OrderService;
 import io.jmix.core.Messages;
 import io.jmix.ui.Notifications;
 import io.jmix.ui.action.Action;
+import io.jmix.ui.component.Collapsable;
 import io.jmix.ui.screen.*;
 import com.company.candyshop.entity.Order;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,12 +28,24 @@ public class OrderEdit extends StandardEditor<Order> {
     @Subscribe("serveCheck")
     public void onServeCheck(Action.ActionPerformedEvent event) {
         Order order = getEditedEntity();
-        if (orderService.checkHaveConfectioneries(order) && (!order.getServeReady())) {
+        boolean checkConfectioneriesFlag = orderService.checkHaveConfectioneries(order);
+        if (checkConfectioneriesFlag && (!order.getServeReady())) {
             orderService.consumeConfectioneries(order);
             order.setServeReady(true);
-        } else notifications.create().withCaption(messages.getMessage("msg://localization/denyOrder.serving"))
+        } else if (checkConfectioneriesFlag) {
+            notifications.create().withCaption(messages.getMessage("localization/denyOrder.serving.ready"))
+                    .withType(Notifications.NotificationType.HUMANIZED)
+                    .show();
+        } else
+            notifications.create().withCaption(messages.getMessage("localization/denyOrder.serving.unReady"))
                 .withType(Notifications.NotificationType.HUMANIZED)
                 .show();
+    }
+
+    @Subscribe("orederedConfectioneriesBox")
+    public void onOrederedConfectioneriesBoxExpandedStateChange(Collapsable.ExpandedStateChangeEvent event) {
+        Order order = getEditedEntity();
+        order.setStartReady(orderService.checkHaveResources(order));
     }
 
     @Subscribe
